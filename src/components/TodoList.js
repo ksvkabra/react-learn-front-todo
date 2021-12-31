ea0f5fac24ee9998f1c849acd902b636e1942e66
Keshav Kabra <ksvkabra@gmail.com>
2021-12-31 22:25:14 +0530

    ✨: add todo list component

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+function TodoList(props) {
+  const todos = props.todos;
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div className='todo__list'>
+        {todos.map(function (todo, index) {
+          return <span key={index}>{todo}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+/* todos = ['new todo', 'todo 1', 'todo 2'] //past
+[<span>new todo</span>, <span>todo 1</span>, <span>todo 2</span>] // new ara
+ */
+export default TodoList;
