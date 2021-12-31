f0a3f748d28f007eb25c3e1535ba2c93eae49ef0
Keshav Kabra <ksvkabra@gmail.com>
2021-12-31 21:03:15 +0530

    chore: change app component to component test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// component - functional component
+
+// component
+// class component
+// function component - create a function
+// pascal case
+function ComponentTest() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 
+      header
+      todos list
+      todo form
+      footer
+    */}
     </div>
   );
 }
 
-export default App;
+export default ComponentTest;
+
+// pascal case - KeshavKabra
+// camelCase - sunilJangid
+// kebab-case - sunil-jangid
